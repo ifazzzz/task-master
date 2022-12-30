9ba856a49a75ba633b1036f69fa74296bd2a63a2
ifazzzz <shoaibahmedifaz@gmail.com>
2022-12-30 15:57:33 +0600

    last commit

--- src/Shared/Footer.js
+++ src/Shared/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
 
@@ -6,6 +7,32 @@ const Footer = () => {
     return (
         <div>
             
+            <footer className="p-4 bg-gray-50 rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
+                        <img src="https://cdn-icons-png.flaticon.com/512/7792/7792148.png" className="mr-3 h-8" alt="Flowbite Logo" />
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Task-Master</span>
+                    </a>
+                    <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <li>
+                            <Link className="mr-4 hover:underline md:mr-6 ">About</Link>
+                        </li>
+                        <li>
+                            <Link className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
+                        </li>
+                        <li>
+                            <Link className="mr-4 hover:underline md:mr-6 ">Licensing</Link>
+                        </li>
+                        <li>
+                            <Link className="hover:underline">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Task-Master™</a>. All Rights Reserved.
+                </span>
+            </footer>
+
         </div>
     );
 };
